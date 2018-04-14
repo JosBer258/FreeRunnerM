9556f00f807014bfca6ef5f4db9d940839a73331
Josias Bertrand <bertrandjosias@gmail.com>
2018-04-14 05:14:14 -0500

    Menu principal y vidas
    mas o menos

---
+++ Assets/Scripts/C_LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class C_LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MostrarEscenea(string FV_SceName)
+    {
+        SceneManager.LoadScene(FV_SceName);
+    }
+
+    public void CerrarAplicacion()
+    {
+        Application.Quit();
+        //Debug.Log("cierre");
+    }
+}
